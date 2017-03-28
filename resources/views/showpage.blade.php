8b957b21f8805273cb26ec424f9305379b6bff35
Arifur Rahman <pavelpatiya@gmail.com>
2017-03-28 11:23:00 +0600

    Initial project upload.
    Its the initial project upload to the hub.

---
+++ resources/views/showpage.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <img src="{{url('')}}/thumb.png" alt="">
+    <script type="text/javascript">
+        window.location = "{{$url}}";//here double curly bracket
+    </script>
+  </body>
+</html>
